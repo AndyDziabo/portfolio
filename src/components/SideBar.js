898a91014f305d709c3bada9507cc60a3ac57920
Andrew Dziabo <jameszabo@yahoo.com>
2023-01-02 21:58:51 -0500

    sidebar links and styling

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,27 @@
-import {BsGithub} from 'react-icons/bs';
-import {AiFillLinkedin} from 'react-icons/ai';
-import {TfiEmail} from 'react-icons/tfi';
+import { BsGithub } from 'react-icons/bs';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { FaDev } from 'react-icons/fa';
+import { TfiEmail } from 'react-icons/tfi';
 
 function SideBar(){
     return(
         <div className="sidebar">
             <div className='sidebar-top'></div>
+            <div className='sidebar-logo-container'>
+                {/* <div className='sidebar-logo-container-inner'> */}
             <div className='sidebar-logo'>
-                <BsGithub />
+                <a href="https://github.com/AndyDziabo" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
             </div>
             <div className='sidebar-logo'>
-                <AiFillLinkedin />
+                <a href="https://www.linkedin.com/in/andrewdziabo/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
             </div>
             <div className='sidebar-logo'>
-                <TfiEmail />
+                <a href="https://dev.to/andydziabo" target="_blank" rel="noopener noreferrer"><FaDev /></a>
+            </div>
+            <div className='sidebar-logo'>
+            <a href="#contact"><TfiEmail /></a>
+            </div>
+                {/* </div> */}
             </div>
         </div>
     )
