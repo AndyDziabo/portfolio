face5dd2bcefd9b4bb8bd91f7da0dc6b93ee81a0
Andrew Dziabo <jameszabo@yahoo.com>
2023-01-16 16:52:28 -0500

    vh variable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import { useState } from 'react';
 
 function App() {
   const [target, setTarget] = useState('');
+
+  let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    
+  });
+  console.log(vh)
   return (
     <div id='app' >
         <NavBar setTarget={setTarget} />
